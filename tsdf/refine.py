7d775193465a2b0ee75d3060c3098ce9e890f963
Peter Hügel <huegelp@gmail.com>
2020-10-12 20:28:46 +0200

    path cleanup

--- tsdf/refine.py
+++ tsdf/refine.py
@@ -330,17 +330,14 @@ class pose_refiner:
         return self.extrinsics_opt
 
 if __name__ == "__main__":
-
-    color_dir = "/home/flo/Documents/3DCVProject/RGBD-SLAM/debug/color_down_png/"
-    # color_dir = "/home/flo/Documents/3DCVProject/RGBD-SLAM/debug/color_full/"
-    depth_dir = "/home/flo/Documents/3DCVProject/RGBD-SLAM/debug/R_hierarchical2_mc/B0.1_R1.0_PL1-0_LR0.0004_BS2_Oadam/depth/"
-    metadata = "/home/flo/Documents/3DCVProject/RGBD-SLAM/debug/R_hierarchical2_mc/metadata_scaled.npz"
-
+    base_dir = "/home/flo/Documents/3DCVProject/RGBD-SLAM/debug/"
     if peter:
-        color_dir = "/home/noxx/Documents/projects/consistent_depth/results/debug03/color_down_png/"
-        # color_dir = "/home/noxx/Documents/projects/consistent_depth/results/debug03/color_full/"
-        depth_dir = "/home/noxx/Documents/projects/consistent_depth/results/debug03/R_hierarchical2_mc/B0.1_R1.0_PL1-0_LR0.0004_BS3_Oadam/depth/"
-        metadata = "/home/noxx/Documents/projects/consistent_depth/results/debug03/R_hierarchical2_mc/metadata_scaled.npz"
+        base_dir = "/home/noxx/Documents/projects/consistent_depth/results/debug03/"
+
+    color_dir = base_dir+"color_down_png/"
+    # color_dir = base_dir+"color_full/"
+    depth_dir = base_dir+"R_hierarchical2_mc/B0.1_R1.0_PL1-0_LR0.0004_BS3_Oadam/depth/"
+    metadata = base_dir+"R_hierarchical2_mc/metadata_scaled.npz"
 
     refiner = pose_refiner(color_dir, depth_dir, metadata)
     refiner.fresh = fresh
